75f0de4b378e873303287278d0fee9ad233bc6c3
chalith <desaman.chalith@gmail.com>
2024-04-17 19:18:23 +0530

    Input handler

--- src/contract.js
+++ src/contract.js
@@ -1,50 +1,45 @@
 const HotPocket = require('hotpocket-nodejs-contract');
 const crypto = require('crypto');
 const fs = require('fs');
+
 const reqsevens = 5;
 
 const tsfile = "timestamps.txt";
 
 const opfile = "../opinion.txt";
 
-function pow(lgrhex, pubkeyhex, sevens)
-{
-	for(let upto = 0n; upto < 0xFFFFFFFFFFFFFFFFn ; upto++)
-	{
+function pow(lgrhex, pubkeyhex, sevens) {
+    for (let upto = 0n; upto < 0xFFFFFFFFFFFFFFFFn; upto++) {
 
-		let uptohex = upto.toString(16);
+        let uptohex = upto.toString(16);
         if (uptohex.length < 16)
-			uptohex = '0'.repeat(16 - uptohex.length) + uptohex;
- 
-		let buf = Buffer.from(lgrhex + pubkeyhex + uptohex, "hex");
+            uptohex = '0'.repeat(16 - uptohex.length) + uptohex;
 
-		let sha = crypto.createHash('sha512').update(buf).digest('hex');
+        let buf = Buffer.from(lgrhex + pubkeyhex + uptohex, "hex");
 
-		let i = 0;
-		for (; i < sevens && i < sha.length; ++i)
-		{
-			if (sha.charCodeAt(i) == 55)
-			{
-				if (i >= sevens - 1)
+        let sha = crypto.createHash('sha512').update(buf).digest('hex');
+
+        let i = 0;
+        for (; i < sevens && i < sha.length; ++i) {
+            if (sha.charCodeAt(i) == 55) {
+                if (i >= sevens - 1)
                     return uptohex;
 
                 // next
-			}
-			else break;
-		}
+            }
+            else break;
+        }
     }
 
     // this failure case will never happen but cover it anyway
     return '0'.repeat(16);
 }
 
-function countsevens(lgrhex, pubkeyhex, uptohex)
-{
+function countsevens(lgrhex, pubkeyhex, uptohex) {
     let buf = Buffer.from(lgrhex + pubkeyhex + uptohex, "hex");
     let sha = crypto.createHash('sha512').update(buf).digest('hex');
 
-    for (let i = 0; i < sha.length; ++i)
-    {
+    for (let i = 0; i < sha.length; ++i) {
         if (sha.charCodeAt(i) != 55)
             return i;
     }
@@ -52,44 +47,54 @@ function countsevens(lgrhex, pubkeyhex, uptohex)
 }
 
 const myContract = async (ctx) => {
-    if (ctx.readonly)
-    {
+    if (ctx.readonly) {
+        // Your smart contract logic.
+        for (const user of ctx.users.list()) {
+            console.log("User public key", user.publicKey);
+
+            // Loop through inputs sent by each user.
+            for (const input of user.inputs) {
+                const buffer = await ctx.users.read(input);
+
+                const message = buffer.toString();
+                const req = JSON.parse(message);
+
+                if (req.command === 'read_scores') {
+                    const output = fs.existsSync(opfile) ? JSON.parse(fs.readFileSync(opfile)) : null;
+                    user.send(JSON.stringify({ message: output }));
+                }
+            }
+        }
 
         return;
     }
 
     fs.appendFileSync(tsfile, ctx.timestamp + "\n");
-    
+
     let good = {};
 
-    ctx.unl.onMessage((node, msg) =>
-    {
+    ctx.unl.onMessage((node, msg) => {
         let sev = countsevens(ctx.lclHash, node.publicKey, msg);
         if (sev >= reqsevens)
             good[node.publicKey] = 1;
     });
-    
+
     await ctx.unl.send(pow(ctx.lclHash, ctx.publicKey, reqsevens));
 
 
     // wait 3 seconds
-    await new Promise((resolve) => 
-    {
-        setTimeout(() =>
-        {
-           // write out our opinions
-       
-            try
-            {
-                if (!fs.existsSync(opfile))
-                {
-                    fs.appendFileSync(opfile, JSON.stringify(good)); 
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            // write out our opinions
+
+            try {
+                if (!fs.existsSync(opfile)) {
+                    fs.appendFileSync(opfile, JSON.stringify(good));
                     return resolve();
                 }
-        
+
                 let ops = JSON.parse(Buffer.from(fs.readFileSync(opfile), 'utf-8'));
-                for (k in good)
-                {
+                for (k in good) {
                     if (k in ops)
                         ops[k]++;
                     else
@@ -102,9 +107,9 @@ const myContract = async (ctx) => {
                 console.error(e);
                 resolve();
             }
-       }, 3000);
+        }, 3000);
     });
 };
 
 const hpc = new HotPocket.Contract();
-hpc.init(myContract);
+hpc.init(myContract, null, true);

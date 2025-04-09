e752914615b38cd482d32bdd3f2d86962649bd06
chalith <desaman.chalith@gmail.com>
2025-04-09 11:07:31 +0530

    Increased the round time

--- dependencies/lobby/index.js
+++ dependencies/lobby/index.js
@@ -45,7 +45,7 @@ function updateHpContract(unl, peers) {
 
     cfg.contract.consensus = {
         ...cfg.contract.consensus,
-        roundtime: 10000,
+        roundtime: 20000,
         threshold: 51
     }
     cfg.contract.unl = unl;
